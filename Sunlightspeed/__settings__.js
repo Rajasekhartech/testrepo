b138b766d6941c6a0e86e34aefb761acdd616ab0
Rajasekhar <rajasekhartechpy@gmail.com>
2021-09-12 15:42:46 +0530

    first commit

---
+++ Sunlightspeed/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1212631.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 53519704, 53519705, 53519706, 53519707, 53519708, 53519724, 53519728, 53519747, 53519892, 53519897, 53519899, 53519901, 53519905, 53519906, 53658071, 54033282, 54121485, 54143138, 54163388, 54303152 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/53519710/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/53519711/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/53519709/1/ammo.js', 'preload' : true},
+];
---
+++ js/main.js
@@ -0,0 +1,43 @@
+function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value+ "; path=/";
+}
+function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+        x = x.replace(/^\s+|\s+$/g, "");
+        if (x == c_name) {
+            return unescape(y);
+        }
+    }
+}
+
+function fullscreenmain(elem){
+      if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+      } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+      } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+      }
+      else if (elem.webkitEnterFullscreen) { /* Iphones */
+        elem.webkitEnterFullscreen();
+      }
+    }
+
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+
+  }
